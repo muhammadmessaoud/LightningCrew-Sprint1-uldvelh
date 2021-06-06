9bf9032d8c959c7d4e98d0994916969077979cd7
Alicia <aliciaderradji@live.fr>
2021-06-06 11:51:53 +0200

    added components layout(header & footer), login, oh-found, inscription, login

--- projet-uldvelh-front/src/app/app.module.ts
+++ projet-uldvelh-front/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { OhFourComponent } from './oh-four/oh-four.component';
+import { LoginComponent } from './login/login.component';
+import { InscriptionComponent } from './inscription/inscription.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    OhFourComponent,
+    LoginComponent,
+    InscriptionComponent
   ],
   imports: [
     BrowserModule,
