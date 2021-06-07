139388b6fffccb3912244b234e3a553c1dbfa110
Alicia <aliciaderradji@live.fr>
2021-06-07 11:14:23 +0200

    delete files on main

--- projet-uldvelh-front/src/app/app-routing.module.ts
+++ projet-uldvelh-front/src/app/app-routing.module.ts
@@ -1,9 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { InscriptionComponent } from './inscription/inscription.component';
-import { LayoutComponent } from './layout/layout.component';
-import { LoginComponent } from './login/login.component';
-import { OhFourComponent } from './oh-four/oh-four.component';
 
 const routes: Routes = [];
 
