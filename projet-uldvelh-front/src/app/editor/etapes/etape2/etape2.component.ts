980c2f5693ca30e0dc19e7cb9bcd9337a1e3198d
Muhammad <messaoudmuhammad9@gmail.com>
2021-06-11 16:40:14 +0200

    Ajout du dossier etapes

---
+++ projet-uldvelh-front/src/app/editor/etapes/etape2/etape2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-etape2',
+  templateUrl: './etape2.component.html',
+  styleUrls: ['./etape2.component.scss']
+})
+export class Etape2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
